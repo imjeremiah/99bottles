5a0d77a2e4856e6c1f3c2935b8b2c0ddff14e242
imjeremiah <52246955+imjeremiah@users.noreply.github.com>
2024-10-24 10:51:50 -0700

    Update verses for a verse range

--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -73,7 +73,7 @@ class BottlesTest < Minitest::Test
       "1 bottle of beer on the wall, " +
       "1 bottle of beer.\n" +
       "Take it down and pass it around, " +
-      "no more bottles of beer on the wall.\n"
+      "no more bottles of beer on the wall.\n" +
       "\n" +
       "No more bottles of beer on the wall, " +
       "no more bottles of beer.\n" +
